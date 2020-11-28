89ded8ccef03ac34f7f229093f8e7bf136b31e47
Lemon Plug <lemon.arjhay0110@gmail.com>
2020-11-28 16:00:48 +0800

    add client side

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const locationMap = document.querySelector('.location-map');
+
+// add something that will fetch the api_key from the backend
+mapboxgl.accessToken = mapToken();
+var map = new mapboxgl.Map({
+    container: locationMap,
+    style: 'mapbox://styles/lemonplug/ckenng4qa0bsl1anzmux3hh43', // stylesheet location
+    center: [120.3136, 14.6297], // starting position [lng, lat]
+    zoom: 15 // starting zoom
+});
+
+
+function mapToken(){
+    
+}
